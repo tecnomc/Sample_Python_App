dd20440b52b226c8be6930fd5007b73673ed4900
shilpa <shilpa.com>
2026-03-05 12:28:06 +0000

    app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Devops Pipeline!"
+    return "Hello from Devops python Pipeline!"
 
 @app.route("/health")
 def health():
