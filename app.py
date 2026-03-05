776f9d9bb7faf34954c5e96508a95f83a9cb340b
shilpa <shilpa@gmail.com>
2026-03-05 12:53:55 +0530

    pythonapp

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from DevOps Pipeline!"
+
+@app.route("/health")
+def health():
+    return "Application is healthy"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
